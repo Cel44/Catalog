56a57d55bc4829c68938407875c20542aeadebb9
Cel44 <celineoei04@gmail.com>
2026-04-20 20:59:13 +0800

    chore: rearrange 'Catalog' position

--- component/header.tsx
+++ component/header.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Header() {
    return (
       <nav className="border-b border-gray-200 bg-pink-300">
-         <div className="container mx-auto flex h-16 items-center px-4 justify-between">
-            <h1 className="flex items-center gap-2 text-2xl font-bold text-white pl-4">
+         <div className="container mx-4 flex h-16 items-center justify-between">
+            <h1 className="flex items-center gap-2 text-2xl font-bold text-white">
                Catalog 
             </h1>
          </div>
